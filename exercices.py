0c8a5773c65d9f399a89e99d8664c3ab67270167
vegavictorj <victorvega@live.ca>
2022-10-21 10:18:30 -0400

    Algorithme pour somme, différence et produit.

---
+++ exercices.py
@@ -0,0 +1,14 @@
+
+
+def operation(x, y, j = 7):
+    
+    somme = x + y + j
+    return somme
+
+    difference = y - j
+    return difference
+
+    produit = x * y
+    return produit
+
+operation(4, 6)
